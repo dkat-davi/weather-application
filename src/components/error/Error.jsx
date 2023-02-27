e10feaa480d7b2b89824ff374662a23e029f76c8
DKAT-DAVI <eudkatdavikalel@gmail.com>
2023-02-27 16:55:47 -0300

    Add color to input in form and import style for error component.

--- src/components/error/Error.jsx
+++ src/components/error/Error.jsx
@@ -1,7 +1,8 @@
+import './style.css'
 
 export function Error() {
     return (
-        <div>
+        <div className='error-container'>
             <p>Não foi possível encontrar o clima de uma cidade com este nome</p>
         </div>
     )
